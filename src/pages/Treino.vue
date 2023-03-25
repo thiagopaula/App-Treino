10625629af0a368a8da72c2b274a0ca8aba791df
thiagopaula <thiagosampaiopaula@gmail.com>
2023-03-25 10:27:54 -0300

    subida de codigo

---
+++ src/pages/Treino.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-page padding>
+    oi
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage'
+})
+</script>
